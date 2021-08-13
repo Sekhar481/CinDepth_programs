aa62ef8c74f98c17f6d25a95734fba4fcb2334f9
Reddi Sekhar Reddy <reddi-sekhar-reddy.kamalapuram@broadcom.com>
2021-08-13 13:55:07 +0530

    First ccheckin

--- Chapter7/P7_1.c
+++ Chapter7/P7_1.c
@@ -1,4 +1,4 @@
-/*P7.1 Program to find the factorial of a number by recursive method*/
+/*P7.1 Program to find the factorial of a number by recursive method Modified the script*/
 #include<stdio.h>
 long int fact(int n);
 long int Ifact(int n);
